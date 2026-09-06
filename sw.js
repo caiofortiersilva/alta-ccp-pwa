dd8d30378693be4e2f010efb66e9caa70207a0b3
caiofortiersilva <fortier.caio@gmail.com>
2026-09-06 08:24:41 -0300

    Atualiza cache da PWA para carregar Alta CCP

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'papelada-ccp-shell-v1';
-const SHELL = ['/', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
+const CACHE = 'alta-ccp-shell-v2';
+const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)));
@@ -7,18 +7,21 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-  if (event.request.method !== 'GET' || url.pathname.startsWith('/api/')) return;
+  if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
-      const copy = resp.clone();
-      caches.open(CACHE).then(cache => cache.put(event.request, copy));
-      return resp;
-    }))
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return resp;
+      }).catch(() => caches.match('./index.html'))
+    )
   );
 });
